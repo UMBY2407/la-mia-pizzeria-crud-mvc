ea705ffb7eca536d9f96189221d423167b1e067b
UMBY2407 <umby2407@gmail.com>
2023-05-18 12:40:03 +0200

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,36 @@
+﻿using LaMiaPizzeria.Database;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> ourTecArticles = db.Pizzas.ToList();
+                return View(ourTecArticles);
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                Pizza? pizzaDetails = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaDetails != null)
+                {
+                    return View("Details", pizzaDetails);
+                }
+                else
+                {
+                    return NotFound($"La pizza con id {id} non è stato trovato!");
+                }
+            }
+
+        }
+    }
+}
